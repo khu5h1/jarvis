71baccf08776baca3a8a7c54ad43673978e8f9ea
khu5h1 <60094678+khu5h1@users.noreply.github.com>
2020-09-30 10:51:16 -0700

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,61 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+
+engine = pyttsx3.init('sapi5')  # to use the voice provided by windows
+voices = engine.getProperty('voices')  # To get the list of voices
+engine.setProperty('voices', voices[0].id)  # to select the voice
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+
+    speak("I am your assistant sir!!! How may i help you?")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening......")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+
+    try:
+        print("Recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)
+        print("say that again please....")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    # takeCommand()
+    # while True:
+    #     query = takeCommand().lower()
+    #     if 'wikipedia' in query:
+    #         speak('Searching Wikipedia.....')
+    #         query = query.replace("Wikipedia", "")
+    #         results = wikipedia.summary(query, Sentences=2)
+    #         speak("According to Wiki")
+    #         speak(results)
+    #     elif 'open youtube' in query:
+    #         webbrowser.open("youtube.com")
+    # executing tasks
